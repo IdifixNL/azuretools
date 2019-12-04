632ab23d1cf13afe9070f5d2039d72bd87f33b99
Nico Baburek <nico.baburek@valtech.com>
2019-12-04 13:28:39 +0100

    Create Managesubcriptions.ps1
    fd

---
+++ Managesubcriptions.ps1
@@ -0,0 +1,16 @@
+﻿#Script Nico Baburek
+
+
+## *Get all subscriptions in all tenants*
+
+#  Get-AzSubscription
+
+
+## *Set active subcription*
+
+#$context = Get-AzSubscription -SubscriptionId ""
+#Set-AzContext $context
+
+
+
+
