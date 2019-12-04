0d734b7a2f4f612b977b511a0243aead9262177f
Nico Baburek <nico.baburek@valtech.com>
2019-12-04 14:56:39 +0100

    Create ManageKeyVault.ps1

---
+++ ManageKeyVault.ps1
@@ -0,0 +1,8 @@
+﻿##Script Nico Baburek 2019##
+
+##Mange the key vault in Azure
+
+
+
+# Get value from Azure key vault in plain text#
+(Get-AzureKeyVaultSecret -VaultName '<>' -name <> ).SecretValueText
